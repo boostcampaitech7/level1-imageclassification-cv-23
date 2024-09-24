84ec78b53c896486faf613a7c3a7407c533c1822
jihyun-0611 <ahi0611@gmail.com>
2024-09-25 01:43:02 +0900

    update sweep train

--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 1. wandb.ai 에서 회원 가입 → API 키 저장 
 2. pip install wandb
-3. wandb_train.py의 main() 에서 sweep_config 설정 (https://docs.wandb.ai/ko/guides/sweeps/define-sweep-configuration)
-4. sweep_run.sh에서 pretrained_model_path에 하이퍼파리마터를 최적화하고 싶은 학습된 모델 파일 위치 경로 저장(.pt)
-5. bash sweep_run.sh
-6. 실행하면 로그인하라고 함 -> API key 입력
+3. sweep_config.yaml에서 파라미터 설정 (https://docs.wandb.ai/ko/guides/sweeps/define-sweep-configuration)
+4. bash sweep_run.sh
+5. 실행하면 로그인하라고 함 -> API key 입력
--- sweep_run.sh
+++ sweep_run.sh
@@ -4,17 +4,19 @@ train_csv_file="/data/ephemeral/home/common_data/data/train"
 traindata_info_file="/data/ephemeral/home/common_data/data/train.csv" 
 save_result_path="/data/ephemeral/home/wandb-test/level1-cv-23/train_result"
 pretrained_model_path="/data/ephemeral/home/wandb-test/level1-cv-23/best_convnext_xxlarge.clip_laion2b_soup_ft_in1k.pt"
+sweep_config_path="sweep_config.yaml"
 
 model_name="convnext_xxlarge.clip_laion2b_soup_ft_in1k"
 img_size=256
 
 echo "Starting hyperparameter optimization for model: $model_name, img_size: $img_size"
 
-python wandb_train.py --traindata_dir "$train_csv_file" \
-                      --traindata_info_file "$traindata_info_file" \
-                      --save_result_path "$save_result_path" \
-                      --model_name "$model_name" \
-                      --img_size "$img_size" \
-                      --pretrained_model_path "$pretrained_model_path"
+# Sweep 생성 및 ID 추출
+sweep_output=$(wandb sweep --project model_optimization $sweep_config_path 2>&1)
+sweep_id=$(echo "$sweep_output" | grep -oP "(?<=wandb agent )[^ ]+")
+sweep_count=5
+
+wandb agent --count $sweep_count $sweep_id
+
 
 echo "Hyperparameter optimization completed."
